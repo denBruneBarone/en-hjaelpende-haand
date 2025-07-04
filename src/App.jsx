594cb16deb8d52ec8ebb2400f802944569f7adc4
denBruneBarone <runeberhardt@gmail.com>
2025-07-04 18:47:35 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 export default function App() {
   return (
     <div className="container mt-5 text-center">
-      <h1 className="display-4">Welcome to Bun + React</h1>
-      <p className="lead">This is your homepage styled with Bootstrap.</p>
+      <h1 className="display-4">Marianne Eberhardt - En Støttende Hån</h1>
+      <p className="lead">Jeg støtter, men håner dig</p>
       <Link to="/kontakt" className="btn btn-primary">Kontakt mig!</Link>
     </div>
   );
